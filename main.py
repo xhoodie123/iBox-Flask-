cebd0ee5cb7be11a79dce6db2117bb0d0390dc3d
xhoodie123 <70179274+xhoodie123@users.noreply.github.com>
2021-10-11 13:00:59 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# located outside of the site folder
+from website import create_app	# website is now a pyton package so by default it will run anything in our init.py file
+
+app = create_app()
+
+if __name__ == "__main__": 	# runs flask app, only if we run this file it will run the server
+	app.run(debug=True)	
+	
