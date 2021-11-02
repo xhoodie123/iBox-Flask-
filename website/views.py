88babdb5475be6fff9fa3400717d62eb1297b4ff
Angel390 <angel.arias48@gmail.com>
2021-11-02 16:13:47 -0700

    manage files section

--- website/views.py
+++ website/views.py
@@ -63,23 +63,20 @@ def upload_file():
             file_ext = os.path.splitext(filename)[1]
             if file_ext not in FILE_EXT: # check if file extension is in whitelist
                 print('invalid file type')
-                flash('File type not allowed', category='error')        
+                flash('File type not allowed', category='error')   
+                return redirect(request.url)  
             file.save(os.path.join(UPLOAD_FOLDER, filename))  # fixed this
             print("Saved file successfully")
-    # send file name as parameter to downlad (may no longer be needed)
-        return redirect('/downloadfile/' + filename)
 
     return render_template('upload_file.html', user=current_user)
 
-# download API section
+# manange file API section
+@views.route('/uploads/<filename>', methods=['GET']) #had to be changed to allow button function
+def manage_file(filename):
+    return render_template('manage_file.html', user=current_user, value=filename)
 
-#depricated!
-@views.route('/downloadfile/<filename>', methods=['GET'])
-def download_file(filename):
-    return render_template('download_file.html', user=current_user, value=filename)
-#end depricated
 
-@views.route('/return-files/<filename>')
+@views.route('/return-files/<filename>') 
 def return_files(filename):
     file_path = 'uploads/' + filename
     if filename == '<filename>':
@@ -87,6 +84,19 @@ def return_files(filename):
         return render_template('home.html', user=current_user)
     return send_file(file_path, as_attachment=True, attachment_filename='')
 # end download api section
+
+#start delete file
+@views.route('/delete-files/<filename>',methods=['GET', 'POST'])
+def delete_files(filename):
+    if filename == '<filename>':
+        flash('File not found!', category='error')
+        return render_template('manage_file.html', user=current_user)
+    os.remove(os.path.join(UPLOAD_FOLDER, filename))
+    print('delete successful')
+    flash('File deleted', category='success')
+    return render_template('uploads.html',user=current_user)
+#end dlete file
+
 
 # start view file list
 @views.route('/', defaults={'req_path': ''})
@@ -98,26 +108,12 @@ def list_file(req_path):
               category='error')  # if path doesn't exist
         return render_template('home.html', user=current_user)
     if os.path.isfile(abs_path):
-        return send_file(req_path, as_attachment=True, attachment_filename='')
+        #return send_file(req_path, as_attachment=True, attachment_filename='')
+        return manage_file(req_path)
     uploads = os.listdir(abs_path)
     return render_template('uploads.html', uploads=uploads, user=current_user)
 # end view file list
 
-#start delete file
-
-@views.route('/deletefile/<filename>', methods=['GET'])
-def delete_file(filename):
-    return render_template('delete_file.html', user=current_user, value=filename)
-
-@views.route('/delete-files/<filename>',methods=['GET', 'POST'])
-def delete_files(filename):
-    if filename == '<filename>':
-        flash('File not found!', category='error')
-        return render_template('home.html', user=current_user)
-    os.remove(os.path.join(UPLOAD_FOLDER, filename))
-    print('delete successful')
-    return render_template('home.html', user = current_user)
-#end dlete file
 
 #start group manager
 @views.route('/group_manager', methods=['GET'])
