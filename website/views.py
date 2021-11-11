383c66b88bec784922b517a4f6b3273174bef537
xhoodie123 <70179274+xhoodie123@users.noreply.github.com>
2021-11-11 15:17:51 -0800

    Update views.py
    trying to properly send data from the uploads folder to shows pdfs in a modal or iframe

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
-from posixpath import splitext
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, g, session
 from flask.helpers import flash, send_from_directory
 from flask_login import login_required, current_user
-from . models import Note, User, Group
-from . import db, UPLOAD_FOLDER, FILE_EXT
+from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
+from . models import User, Role, UserRoles
+from . import db, UPLOAD_FOLDER, migrate, FILE_EXT
 import json  # for delete note
 # for securing uploaded file/download file
 from werkzeug.utils import secure_filename, send_file
@@ -13,37 +13,54 @@ import os
 views = Blueprint("views", __name__)
 
 
-@views.route("/", methods=['GET', 'POST'])  # Note post Method is allowed
-@login_required  # cannot get to homepage if not logged in
+
+
+
+
+@views.route('/')
 def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-        if len(note) < 1:
-            flash('Note is short', category='error')
-        else:
-            # add note to database and associate with user
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added', category='success')
-    # ref current user to check if logged in
-    return render_template("home.html", user=current_user)
+	return render_template('home.html')
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    # turn string  into python dictionary object
-    note = json.loads(request.data)
-    noteId = note['noteId']  # access note id
-    note = Note.query.get(noteId)  # find the note
-    if note:  # if note belongs to the user
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-    return jsonify({})  # return empty response
+@views.route('/member')
+@login_required
+def profile():
+	return_template ('profile.hmtl')
 
 
-@views.route('/uploadfile', methods=['GET', 'POST'])
+
+@views.route("/sign-in")
+@login_required  # can only logout in logged in
+def logout():
+    return redirect(url_for('user.logout'))
+
+
+
+
+@views.route('/profile-page', methods = ['GET', 'POST'])
+@login_required
+def user_profile_page():
+    # Initialize form
+    form = UserProfileForm(request.form, obj=current_user)
+
+    # Process valid POST
+    if request.method == 'POST' and form.validate():
+        # Copy form fields to user_profile fields
+        form.populate_obj(current_user)
+
+        # Save user_profile
+        db.session.commit()
+
+        # Redirect to home page
+        return redirect(url_for('home'))
+
+    # Process GET or invalid POST
+    return render_template('profile.html',
+                           form=form)
+
+
+
+@views.route('/upload-file', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -68,52 +85,56 @@ def upload_file():
             print("Saved file successfully")
 
     return render_template('upload_file.html', user=current_user)
+    
+#@views.route('/uploads/<filename>', methods=['GET']) 
+#def manage_file(filename):
+#    return render_template('manage_file.html', user=current_user, value=filename)
+    
+#@views.route('/uploads', methods=['GET'])
+#def file_viewer():
 
-@views.route('/uploads/<filename>', methods=['GET']) # had to be changed to allow button function
-def manage_file(filename):
-    return render_template('manage_file.html', user=current_user, value=filename)
-
-
-@views.route('/return-files/<filename>')
-def return_files(filename):
-    file_path = 'uploads/' + filename
-    if filename == '<filename>':
-        flash('File not found!', category='error')
-        return render_template('home.html', user=current_user)
-    return send_file(file_path, as_attachment=True, attachment_filename='')
-
-@views.route('/delete-files/<filename>')
-def delete_files(filename):
-    if filename == '<filename>':
-        flash('File not found!', category='error')
-        return render_template('manage_file.html', user=current_user)
-    os.remove(os.path.join(UPLOAD_FOLDER, filename))
-    print('delete successful')
-    flash('File deleted', category='success')
-    return render_template('uploads.html', user=current_user)
-
+#	workingdir = os.path.abspath(os.getcwd())
+#	filepath = workingdir + UPLOAD_FOLDER
+#	return send_from_directory(directory=filepath, path =)
+#	return render_template('uploads.html', user=current_user)
+	
 @views.route('/', defaults={'req_path': ''})
 @views.route('/<path:req_path>')
 def list_file(req_path):
-    # joining the base and requested path
+     #joining the base and requested path
+    filename = '<filename>'
     abs_path = os.path.join('website/', req_path)
     if not os.path.exists(abs_path):
         flash('No files have been uploaded!',
               category='error')  # if path doesn't exist
         return render_template('home.html', user=current_user)
     if os.path.isfile(abs_path):
-        return manage_file(req_path)
+        return return_files_tut('sample.pdf')
     uploads = os.listdir(abs_path)
     return render_template('uploads.html', uploads=uploads, user=current_user)
 
+
+@views.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = 'uploads/' + filename
+    if filename == '<filename>':
+        flash('File not found!', category='error')
+        return render_template('home.html', user=current_user)
+    return send_from_directory(file_path, as_attachment=True, path=filename)
+# end download api section
+
+
+
+# end view file list
+
+
+#flask app to display pdf on the browser
+
+ 
+
+
+#start group manager
 @views.route('/group_manager', methods=['GET'])
 def group_manager():
     return render_template('group_manager.html', user=current_user)
-
-@views.route('/create_group', methods=['GET'])
-def create_group():
-    return render_template('create_group.html', user=current_user)
-
-@views.route('/join_group', methods=['GET'])
-def join_group():
-    return render_template('join_group.html', user=current_user)
+#end group manager
