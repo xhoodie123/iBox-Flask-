7ad2b8b9356e5f190f173f9b42eb8c82dd4e6d52
Angel <angel.arias48@gmail.com>
2021-11-19 17:07:27 -0800

    login/signup page
    works now

--- website/auth.py
+++ website/auth.py
@@ -17,31 +17,62 @@ auth = Blueprint("auth", __name__)  # defines blueprint
 # defines our login page
 
 # URL -> GET request, POST -> Submit button
-@auth.route("/login", methods=['GET', 'POST'])
+@auth.route("/startup-page", methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+    if "form-submit" in request.form:
+        if request.method == 'POST':
+            emailLogin = request.form.get('email-login')
+            passwordLogin = request.form.get('password-login')
 
-        # filter all the users by email and each user mush have a unique email
-        user = User.query.filter_by(email=email).first()
-        if user:
-            # if these hashes are the same
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully', category='success')
-                login_user(user, remember=True)  # keeps user logged in
-                # takes user to their home page
-                return redirect(url_for('views.home'))
+            # filter all the users by email and each user mush have a unique email
+            user = User.query.filter_by(email=emailLogin).first()
+            if user:
+                # if these hashes are the same
+                if check_password_hash(user.password, passwordLogin):
+                    flash('Logged in successfully', category='success')
+                    login_user(user, remember=True)  # keeps user logged in
+                    # takes user to their home page
+                    return redirect(url_for('views.home'))
+                else:
+                    flash('Incorrect password', category='error')
             else:
-                flash('Incorrect password', category='error')
-        else:
-            flash('Email does not exist', category='error')
-    data = request.form  # Access form attribute of the request
-    print(data)
-    # displays basic text on site with <p> tags </p>
-    return render_template("login.html", user=current_user)
-# text is a variable we can access within login.html template
+                flash('Email does not exist', category='error')
+        data = request.form  # Access form attribute of the request
+        print(data)
+        # displays basic text on site with <p> tags </p>
+    if "form2-submit" in request.form:
+        if request.method == 'POST':
+            email = request.form.get('email')  # gets all user input for user info
+            first_name = request.form.get('firstName')
+            password1 = request.form.get('password1')
+            password2 = request.form.get('password2')
 
+            user = User.query.filter_by(email = email).first()
+            if user:
+                flash('Email already exists!', category = 'error')
+            if len(email) < 4:
+                flash('Email too short!', category='error')
+            elif len(first_name) < 2:
+                flash('First name must be greater than one character!', category='error')
+            elif password1 != password2:
+                flash('Passwords do not match!', category='error')
+            elif len(password1) < 7:
+                flash('Password too short!', category='error')
+            else:
+                new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                    password1, method='sha256'))  # creates a new user
+                if 0 == 2:
+                    flash('Account already created, login!', category= 'error') #placeholder for future check function
+                else:
+                    db.session.add(new_user)  # adds the new user to the database
+                    db.session.commit()  # tell the database to update
+                    # keep newly created user logged in
+                    login_user(new_user, remember=True)
+                    flash('Account Created!', category='success')
+                    # takes the new user to the home page
+                    return redirect(url_for('views.home'))
+    return render_template("startup_page.html", user=current_user)
+# text is a variable we can access within login.html template
 
 # defines user log out page
 @auth.route("/logout")
@@ -49,49 +80,6 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
-
-
-# defines user sign up page
-@auth.route("/sign-up", methods=['GET', 'POST'])
-def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')  # gets all user input for user info
-        first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        user = User.query.filter_by(email = email).first()
-        if user:
-            flash('Email already exists!', category = 'error')
-        if len(email) < 4:
-            flash('Email too short!', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than one character!', category='error')
-        elif password1 != password2:
-            flash('Passwords do not match!', category='error')
-        elif len(password1) < 7:
-            flash('Password too short!', category='error')
-        else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))  # creates a new user
-            if 0 == 2:
-                flash('Account already created, login!', category= 'error') #placeholder for future check function
-            else:
-                db.session.add(new_user)  # adds the new user to the database
-                db.session.commit()  # tell the database to update
-                # keep newly created user logged in
-                login_user(new_user, remember=True)
-                flash('Account Created!', category='success')
-                # takes the new user to the home page
-                return redirect(url_for('views.home'))
-    # displays the template created from our sign_up.html file
-    return render_template("sign_up.html", user=current_user)
-
-
-@auth.route("/startup-page", methods=['GET', 'POST'])
-def test():
-    return render_template("startup_page.html", user=current_user)
-
 
 # NEW AREA FOR THE OTHER ENGINEER PLEASE DO NOT UNCOMMENT #
 
@@ -135,5 +123,3 @@ def test():
 #		user.roles.append(role)
 #	db.session.add(user)
 #	return user	
-	
-
